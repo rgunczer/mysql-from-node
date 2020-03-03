185816f1d24e7d846a30f85b0bc997faf33bca4f
rgunczer <rgunczer@gmail.com>
2020-03-03 22:41:48 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '*****',
+    database: 'sys'
+});
+
+console.log('connecting...');
+connection.connect(err => {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+});
+
+connection.query('SELECT * FROM `users`', (error, results, fields) => {
+    // error will be an Error if one occurred during the query
+    // results will contain the results of the query
+    // fields will contain information about the returned results fields (if any)
+    console.log(error);
+    console.log(results);
+    console.log(fields);
+});
+
+connection.end(err => {
+    console.log('The connection is terminated now');
+});
